4200b358012ae05472727b922f5b4050d4cb6cc7
Radiy <radiyars@yandex.ru>
2023-08-02 16:12:57 +0300

    поправил стилизацию главного окна, начал добавлять контакты

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
-let arrow = document.querySelectorAll('.arrow');
+const arrow = document.querySelectorAll('.arrow')
+
 for (i = 0; i < arrow.length; i++) {
 	let subMenu = arrow[i].nextElementSibling;
 	let thisArrow = arrow[i];
@@ -8,4 +9,18 @@ for (i = 0; i < arrow.length; i++) {
 		subMenu.classList.toggle('skill__open');
 		thisArrow.classList.toggle('arrow__active');
 	});
+}
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
+		const blockId = anchor.getAttribute('href')
+		document.querySelector('' + blockId).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
 }
